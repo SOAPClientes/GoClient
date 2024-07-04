61a07ca80b84112a7676dcc4d2608e3bdfc6d218
SebastianCotrina16 <sebastiancotrina16@gmail.com>
2024-07-04 12:02:45 -0500

    Go Final

--- main.go
+++ main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"encoding/xml"
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"strings"
 	"time"
+	"fmt"
+	"strconv"
 
 	"github.com/tiaguinho/gosoap"
 )
 
-// Estructuras para almacenar las respuestas SOAP
 type MensajeResponse struct {
 	XMLName       xml.Name `xml:"MensajeResponse"`
 	MensajeResult string   `xml:"MensajeResult"`
@@ -31,6 +32,19 @@ type TablaResponse struct {
 	XMLName      xml.Name `xml:"TablaResponse"`
 	TablaResult  []string `xml:"TablaResult>string"`
 }
+
+type EstudiantesResponse struct {
+	XMLName             xml.Name `xml:"EstudiantesResponse"`
+	EstudiantesResult   []string `xml:"EstudiantesResult>string"`
+}
+
+type Estudiantes2Response struct {
+	XMLName           xml.Name `xml:"Estudiantes2Response"`
+	Estudiantes2Result struct {
+		String []string `xml:"string"`
+	} `xml:"Estudiantes2Result"`
+}
+
 
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
@@ -153,13 +167,123 @@ func tablaHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "tabla.html", nil)
 }
 
+func estudiantesHandler(w http.ResponseWriter, r *http.Request) {
+	httpClient := &http.Client{
+		Timeout: 15 * time.Second,
+	}
+	soap, err := gosoap.SoapClient("https://soapserviceserver.azurewebsites.net/WebServices/WebServer.asmx?WSDL", httpClient)
+	if err != nil {
+		log.Fatalf("SoapClient error: %s", err)
+	}
+
+	params := gosoap.Params{}
+	res, err := soap.Call("Estudiantes", params)
+	if err != nil {
+		log.Fatalf("Call error: %s", err)
+	}
+
+	var response EstudiantesResponse
+	err = xml.Unmarshal(res.Body, &response)
+	if err != nil {
+		log.Fatalf("xml.Unmarshal error: %s", err)
+	}
+
+	students := make([][]string, len(response.EstudiantesResult))
+	for i, student := range response.EstudiantesResult {
+		studentData := strings.Split(student, ",")
+		if len(studentData) < 3 {
+
+			for len(studentData) < 3 {
+				studentData = append(studentData, "")
+			}
+		}
+
+		studentWithNumber := append([]string{fmt.Sprintf("%d", i+1)}, studentData...)
+		students[i] = studentWithNumber
+	}
+
+	for i, student := range students {
+		nota := student[3]
+		estado := "Desaprobado"
+		if notaFloat, err := strconv.ParseFloat(nota, 64); err == nil {
+			if notaFloat >= 10.5 {
+				estado = "Aprobado"
+			}
+		}
+		students[i] = append(student, estado)
+	}
+
+	templates.ExecuteTemplate(w, "estudiantes.html", students)
+}
+
+func estudiantes2Handler(w http.ResponseWriter, r *http.Request) {
+	httpClient := &http.Client{
+		Timeout: 15 * time.Second,
+	}
+	soap, err := gosoap.SoapClient("https://soapserviceserver.azurewebsites.net/WebServices/WebServer.asmx?WSDL", httpClient)
+	if err != nil {
+		log.Fatalf("SoapClient error: %s", err)
+	}
+
+	params := gosoap.Params{}
+	res, err := soap.Call("Estudiantes2", params)
+	if err != nil {
+		log.Fatalf("Call error: %s", err)
+	}
+
+	var response Estudiantes2Response
+	err = xml.Unmarshal(res.Body, &response)
+	if err != nil {
+		log.Fatalf("xml.Unmarshal error: %s", err)
+	}
+
+	// Asegurémonos de que estamos obteniendo los datos correctos
+	log.Printf("Raw response: %+v\n", res.Body)
+	log.Printf("Deserialized response: %+v\n", response)
+
+	students := make([][]string, len(response.Estudiantes2Result.String))
+	for i, student := range response.Estudiantes2Result.String {
+		studentData := strings.Split(student, ",")
+		if len(studentData) < 3 {
+			// Si faltan datos, llenamos con cadenas vacías
+			for len(studentData) < 3 {
+				studentData = append(studentData, "")
+			}
+		}
+		// Asignar número incremental
+		studentWithNumber := append([]string{fmt.Sprintf("%d", i+1)}, studentData...)
+		students[i] = studentWithNumber
+	}
+
+	// Añadir estado basado en la nota
+	for i, student := range students {
+		nota := student[3]
+		estado := "Desaprobado"
+		if notaFloat, err := strconv.ParseFloat(nota, 64); err == nil {
+			if notaFloat >= 10.5 {
+				estado = "Aprobado"
+			}
+		}
+		students[i] = append(student, estado)
+	}
+
+	// Registro de depuración para verificar los datos
+	log.Printf("Processed students: %+v\n", students)
+
+	templates.ExecuteTemplate(w, "estudiantes2.html", students)
+}
+
+
+
 func main() {
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/mensaje", mensajeHandler)
 	http.HandleFunc("/informacion", informacionHandler)
 	http.HandleFunc("/operaciones", operacionesHandler)
 	http.HandleFunc("/tabla", tablaHandler)
+	http.HandleFunc("/estudiantes", estudiantesHandler)
+	http.HandleFunc("/estudiantes2", estudiantes2Handler)
 
-	fmt.Println("Server started at :8084")
+	log.Println("Server started at :8084")
 	log.Fatal(http.ListenAndServe(":8084", nil))
 }
